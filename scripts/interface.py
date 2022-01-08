cb32bedd16501c5bce40eaa9c110ee447d170ea7
Zoe Kim <zoek2100@gmail.com>
2022-01-08 16:19:05 +0900

    feat : display image and video

--- scripts/interface.py
+++ scripts/interface.py
@@ -1,14 +1,20 @@
 from tkinter import *
+import cv2 #openCV
+import dlib
 
 def takeImage(window):
     imageLabel = Label(window, text = "Type the full path for your image", font = ("Arial Bold", 20))
     imageLabel.grid(column=0, row=0)
 
-    imagepath = Entry(window,width=10)
-    imagepath.grid(column=1, row=0)
+    imagePath = Entry(window,width=10)
+    imagePath.grid(column=1, row=0)
 
     clickButton1 = Button(window, text = "Load", command = loadImage(imageLabel))
     clickButton1.grid(column=2, row=0)
+
+    img = cv2.imread(imagePath) 
+    cv2.imshow("Selected image", img) #display 
+    cv2.waitKey(0)
 
 def takeVideo(window):
     videoLabel = Label(window, text = "Type the full path for your video", font = ("Arial Bold", 20))
@@ -20,11 +26,21 @@ def takeVideo(window):
     clickButton2 = Button(window, text = "Load", command = loadVideo(videoLabel))
     clickButton2.grid(column=2, row=5)
 
-def loadImage(imageLabel): #button click
+    vid = cv2.VideoCapture(videoPath)  
+
+    if not vid.isOpened():
+        print('The program failed to open the video\n') #add error message 
+
+    while vid.isOpened():
+        ret, frame = vid.read()
+        if ret:
+            cv2.imshow("Selected video", frame)  # display the image
+            cv2.waitKey(0)
+
+def loadImage(imageLabel): 
     imageLabel.configure(text= "Your image was successfully loaded")
 
-
-def loadVideo(videoLabel): #button click
+def loadVideo(videoLabel):
     videoLabel.configure(text= "Your video was successfully loaded")
 
 def main():
