a4b917c8f81bb00674de1440dd4088148b2c2149
Zoe Kim <zoek2100@gmail.com>
2022-01-30 13:14:36 +0900

    feat : add logo and buttons

--- scripts/main.py
+++ scripts/main.py
@@ -1,56 +1,29 @@
 from tkinter import *
-import cv2
-import interface
+from PIL import Image, ImageTk
+
+HEIGHT = 800
+WIDTH = 800
 
 window = Tk()
 window.title("SSMILE")
-window.geometry('500x300')
-window['background'] = '#F5FF52'
 
-f = ("Times bold", 14)
+frame = Frame(window)
+frame.pack()
 
-def nextPage():
-    window.destroy()
-    import face_page1
+canvas = Canvas(frame, bg = "white", width = WIDTH, height = HEIGHT)
+canvas.pack()
 
-def prevPage():
-    window.destroy()
-    #import audio_page2
-    
-Label(
-    window,
-    text = 'SSMILE',
-    bg = '#F5FF52',
-    font = ("Times bold", 30)
-).pack(expand=True, fill=BOTH)
+canvas.create_text(WIDTH/2, 50, fill = "Black", font = "Roboto 35 bold", text = "SSMILE")
+canvas.create_text(WIDTH/2, 95, fill = "Black", font = "Roboto 20 bold", text = "Stroke Screening Made Instant, Live, and Easy :)")
 
-Label(
-    window,
-    text = 'Stroke Screening Made Instant, Live, and Easy :)',
-    bg = '#F5FF52',
-    font = ("Times bold", 20)
-).pack(expand=True, fill=BOTH)
+photoimage = ImageTk.PhotoImage(file="/Users/zoekim/Desktop/git_SSMILE/SSMILE/scripts/smile_emoji3.png")
+canvas.create_image(WIDTH/2, HEIGHT/2, anchor="center", image=photoimage)
 
-Button(
-    window,
-    text = "Previous Page", 
-    font = f,
-    command = nextPage
-    ).pack(fill=X, expand=TRUE, side=LEFT)
-
-Button(
-    window, 
-    text = "Next Page", 
-    font = f,
-    command = prevPage
-    ).pack(fill=X, expand=TRUE, side = RIGHT)
-
-#path = StringVar()
-
-#interface.takeImage(window, path)
+btn1 = Button(window, text = 'Welcome to SSMILE!',  highlightbackground='white')
+btn2 = Button(window, text = 'Face Detection',  highlightbackground='white')
+btn3 = Button(window, text = 'Audio Test',  highlightbackground='white')
+btn1.place(x = 400, y = 700, anchor="center")
+btn2.place(x = 400, y = 730, anchor="center")
+btn3.place(x = 400, y = 760, anchor="center")
 
 window.mainloop()
-
-#img_file_path = '/Users/minjeongkim/Desktop/SSMILE/scripts/human_face.jpg'
-#face_detection.main(img_file_path)
-
