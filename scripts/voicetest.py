2531a1b41ab0db95238e92b2090715bade9da9eb
Zoe Kim <zoek2100@gmail.com>
2022-01-31 13:23:03 +0900

    feat : calculate similarity between two sentences

--- scripts/voicetest.py
+++ scripts/voicetest.py
@@ -1,9 +1,14 @@
 import speech_recognition as sr
+from difflib import SequenceMatcher
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
   
 r = sr.Recognizer() 
-#audio_file
+audio_file = sr.AudioFile('output.wav')
 
-with audio_file as s: 
-    r.adjust_for_ambient_noise(s) 
-    audio = r.record(s)
-    
+with audio_file as source: 
+    r.adjust_for_ambient_noise(source) 
+    audio = r.record(source)
+
+r.recognize_google(audio)
