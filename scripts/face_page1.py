3edcf1f0049746209eaffc04670327d4f2c13b7a
Zoe Kim <zoek2100@gmail.com>
2022-01-31 13:22:04 +0900

    feat : add web cam option

--- scripts/face_page1.py
+++ scripts/face_page1.py
@@ -1,6 +1,8 @@
 from tkinter import *
+import tkinter
 from tkinter.filedialog import askopenfile
 from PIL import Image, ImageTk
+import cv2
 
 #face detection page (image)
 
@@ -21,7 +23,7 @@ def open_file():
         file = askopenfile(parent = window, mode='rb', title = "Choose a file", filetypes=[("Image File",'.jpg')])
 
 canvas.create_text(WIDTH/2, 50, fill = "Black", font = "Roboto 35 bold", text = "Face Detection")
-canvas.create_text(WIDTH/2, 95, fill = "Black", font = "Roboto 20 bold", text = "Type the full path for your image")
+canvas.create_text(WIDTH/2, 95, fill = "Black", font = "Roboto 20", text = "Click your options below (Image/Webcam)")
 
 load_text = StringVar()
 
@@ -29,12 +31,33 @@ load_button = Button(window,
         textvariable = load_text,
         command = lambda:open_file(),
         highlightbackground = 'white',
-        fg = "black",
-        height=2,
-width=15)
+        width=15)
 
-load_text.set("Load")
+load_text.set("Image Load")
 
-load_button.place(x = 400, y = 700, anchor = "center")
+load_button.place(x = 400, y = 600, anchor = "center")
+
+def open_web_cam():
+        capture = cv2.VideoCapture(0)
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+        while cv2.waitKey(33) < 0:
+                ret, frame = capture.read()
+                cv2.imshow("Face Detection (Webcam)", frame)
+
+        capture.release()
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+        return 
+
+web_cam_button = Button(window,
+        text = 'Webcam', 
+        command = lambda:open_web_cam(),
+        highlightbackground = 'white',
+        width=15)
+
+web_cam_button.place(x = 400, y = 680, anchor = "center")
 
 window.mainloop()
+
