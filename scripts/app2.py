c6310df50d20cd1f3879198c8eb03281b2835a91
Zoe Kim <zoek2100@gmail.com>
2022-02-25 18:08:08 -0500

    fix : inputstream number of channels

--- scripts/app2.py
+++ scripts/app2.py
@@ -1,12 +1,38 @@
 import streamlit as st
+import sounddevice as sd
+from scipy.io.wavfile import write
 
 def run_audio_evaluation():
-    st.title('Audio Evaluation')
+    st.subheader('Audio Evaluation')
+    st.checkbox('Upload a recording of yourself reading the following sentence aloud')
+    st.checkbox('Sample Setence : Nice to meet you. What is your name?')
+    option = st.selectbox(
+     'File Type',
+     ('Select your option', 'Upload', 'Recording'))
 
-    audio_file = st.file_uploader('Upload Your Audio', type = ['wav', 'mp3'])
-
-    if audio_file is None:
+    if option == 'Select your option':
         pass
-    else:
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format='audio/ogg')
+
+    if option == 'Upload':
+        audio_file = st.file_uploader('Upload Your Audio', type = ['wav', 'mp3'])
+
+        if audio_file is None:
+            pass
+        else:
+            audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format='audio/ogg')
+
+    if option == 'Recording':
+        if st.button("Start Recording"):
+            fs = 44100
+            duration = 10.5
+            st.success("recording...............")
+
+            #channel error
+            recording_file = sd.rec(int(duration * fs), samplerate = fs, channels = 2)
+            sd.wait()       
+            write("sound.wav",fs , recording_file)
+
+    return 
+
+
--- scripts/voicetest.py
+++ scripts/voicetest.py
@@ -2,13 +2,15 @@ import speech_recognition as sr
 from difflib import SequenceMatcher
 
 def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
-  
-r = sr.Recognizer() 
-audio_file = sr.AudioFile('output.wav')
+        return SequenceMatcher(None, a, b).ratio()
 
-with audio_file as source: 
-    r.adjust_for_ambient_noise(source) 
-    audio = r.record(source)
+def evaluate_audio(file_name):
+    
+    r = sr.Recognizer() 
+    audio_file = sr.AudioFile(file_name)
 
-r.recognize_google(audio)
+    with audio_file as source: 
+        r.adjust_for_ambient_noise(source) 
+        audio = r.record(source)
+
+    r.recognize_google(audio)
