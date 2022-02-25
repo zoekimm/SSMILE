31b6deb989ca331b053c4ec6126015a31f1938c9
Zoe Kim <zoek2100@gmail.com>
2022-02-24 19:21:17 -0500

    style : font size and color

--- scripts/app.py
+++ scripts/app.py
@@ -16,6 +16,14 @@ def main():
     load_css("style.css")
     if choice == 'Home':
         st.header('Stroke Screening Made Instant, Live, and Easy')
+        st.subheader('According to CDC,')
+        st.checkbox('In 2018, 1 in every 6 deaths from cardiovascular disease was due to stroke.')
+        st.checkbox('Someone in the United States has a stroke every 40 seconds.')
+        st.checkbox('Stroke is a leading cause of serious long-term disability.')
+        st.checkbox('Stroke reduces mobility in more than half of stroke survivors age 65 and over.')
+
+        st.write("--")
+
         st.subheader('Why SSMILE?')
         info_text = """<div>With more than
             <span class='highlight red'>fifteen million</span>
@@ -24,10 +32,10 @@ def main():
             <span class='highlight yellow'> early detection of stroke is significant 
             to prevent any further brain damage and to possibly decrease both mortality and morbidity.</span></span>
             </div>"""
+
         st.markdown(info_text, unsafe_allow_html=True)
-        st.markdown("""This web application helps one determine whether one has facial asymmetry, one of the most common symptoms of stroke, with an image of one's face or through web camera""")
-
-
+        st.markdown("""👉 This web application helps one determine whether one has facial droop and speech problems
+        so that one can act FAST to """)
 
     elif choice == 'Face Detection':
         run_face_detection()
