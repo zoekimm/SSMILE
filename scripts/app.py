616084079cfaac9852db03a3c2168256fcc955fa
Zoe Kim <zoek2100@gmail.com>
2022-02-25 22:33:50 -0500

    style : change theme

--- scripts/app.py
+++ scripts/app.py
@@ -10,21 +10,26 @@ from css import load_css
 def main():
 
     st.title('SSMILE')
-    pages = ['Home', 'Face Detection', 'Audio Evaluation']
+    pages = ['Home', 'Face Detection', 'Audio Evaluation', 'More Information']
     choice = st.sidebar.radio('Select Pages', pages)
 
     load_css("style.css")
+    
     if choice == 'Home':
-        st.header('Stroke Screening Made Instant, Live, and Easy')
-        st.subheader('According to CDC,')
-        st.checkbox('In 2018, 1 in every 6 deaths from cardiovascular disease was due to stroke.')
-        st.checkbox('Someone in the United States has a stroke every 40 seconds.')
-        st.checkbox('Stroke is a leading cause of serious long-term disability.')
-        st.checkbox('Stroke reduces mobility in more than half of stroke survivors age 65 and over.')
+        st.subheader('Stroke Screening Made Instant, Live, and Easy')
+
+        st.subheader('Why SSMILE?')
+
+        statistics_text = """<div>According to CDC, In 2018,
+            <span class='highlight red'>1 in every 6 deaths</span>
+            from cardiovascular disease was due to stroke. Someone in the United States has a stroke
+            <span class='bold'> every 40 seconds</span>. Stroke is a <span class='highlight yellow'> leading cause </span> of serious long-term disability.</span>
+            </div>"""
+
+        st.markdown(statistics_text, unsafe_allow_html=True)
 
         st.write("--")
 
-        st.subheader('Why SSMILE?')
         info_text = """<div>With more than
             <span class='highlight red'>fifteen million</span>
             cases of <span class='bold'>stroke</span> worldwide every year and many more 
@@ -37,10 +42,33 @@ def main():
         st.markdown("""👉 This web application helps one determine whether one has facial droop and speech problems
         so that one can act FAST to """)
 
+        st.write("--")
+
+        #st.caption('According to CDC,')
+        #st.checkbox('In 2018, 1 in every 6 deaths from cardiovascular disease was due to stroke.')
+        #st.checkbox('Someone in the United States has a stroke every 40 seconds.')
+        #st.checkbox('Stroke is a leading cause of serious long-term disability.')
+        #st.checkbox('Stroke reduces mobility in more than half of stroke survivors age 65 and over.')
+
+        st.markdown('More information can be found:')
+
+        github = 'https://github.com/zoekimm/SSMILE'
+        devpost = 'https://devpost.com/software/ssmile'
+        st.markdown("[Github repository](%s)" % github)
+        st.markdown("[DevPost](%s)" % devpost)
+
+
+
+        st.write("--")
+
     elif choice == 'Face Detection':
         run_face_detection()
+
     elif choice == 'Audio Evaluation':
         run_audio_evaluation()
+
+    elif choice == 'More Information':
+        pass
 
 if __name__ == '__main__':
     main()
